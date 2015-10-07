bad6dfd0b123ad402a9e00f3698019f7b88d2c21
Markus Bergholz <markuman@gmail.com>
2015-10-07 20:24:00 +0200

    inputParse failed for CaseSensitive

--- mlint.m
+++ mlint.m
@@ -1,4 +1,13 @@
-function mlint(filename)
+function mlint(filename, varargin)
+
+  # input check
+  p               = inputParser();
+  p.CaseSensitive = true;
+  p.FunctionName  = "mlint"; 
+  p.addOptional ("CodeIndent", true, @isbool);
+  p.addOptional ("ShadowFnc" , true, @isbool);
+  p.addOptional ("UnusedVar" , true, @isbool);
+  p.parse (varargin{:});
   
   # list of functions, variables etc comes from about()
   ret     = about(filename);
@@ -11,6 +20,7 @@ function mlint(filename)
   UL = cellfun(@length, U);
   fseek (fid, marker, SEEK_SET);
   
+  # init counts
   lint = struct();
   lint.line = 0;
   lint.indent = struct();
@@ -31,41 +41,47 @@ function mlint(filename)
     
     ## check for code indent
     ## ---------------------
-    lint.indent.is = regexp(lineOfCode, '^(\s+).*?', 'tokens');
-    if ~isempty(lint.indent.is)
-      lint.indent.is = length(cell2mat(lint.indent.is{1}));
-    else
-      lint.indent.is = 0;
-    endif
-
-    if length(lineOfCode) > 0 && ~all(uint8(lineOfCode) == 32)
-      [~, currentIndentFactor] = CheckforIndent(strtrim(lineOfCode), lint.indent.size);
-      if (lint.indent.is < (lint.indent.next + currentIndentFactor))
-        fprintf("%s:%d - code indent is %d, should be at least %d\n", filename, lint.line, lint.indent.is, lint.indent.next)
+    if (p.Results.CodeIndent)
+      lint.indent.is = regexp(lineOfCode, '^(\s+).*?', 'tokens');
+      if ~isempty(lint.indent.is)
+        lint.indent.is = length(cell2mat(lint.indent.is{1}));
+      else
+        lint.indent.is = 0;
       endif
+
+      if length(lineOfCode) > 0 && ~all(uint8(lineOfCode) == 32)
+        [~, currentIndentFactor] = CheckforIndent(strtrim(lineOfCode), lint.indent.size);
+        if (lint.indent.is < (lint.indent.next + currentIndentFactor))
+          fprintf("%s:%d - code indent is %d, should be at least %d\n", filename, lint.line, lint.indent.is, lint.indent.next)
+        endif
+      endif
+      lint.indent.next = CheckforIndent(strtrim(lineOfCode), lint.indent.size) + lint.indent.is;
     endif
-    lint.indent.next = CheckforIndent(strtrim(lineOfCode), lint.indent.size) + lint.indent.is;
     ## end check for code indent
     ############################
 
     ## check for shadowed functions
     ## -----------------------------
-    if ~isempty(ret.variables.overloaded)
-      [pos, ind] = CheckVariable(lineOfCode, ret.variables.overloaded);
-      if ~isempty(pos)
-        fprintf("%s:%d:%d - variable shadows function: %s\n", filename, lint.line, pos, ret.variables.overloaded{ind})
-      end
+    if (p.Results.ShadowFnc)
+      if ~isempty(ret.variables.overloaded)
+        [pos, ind] = CheckVariable(lineOfCode, ret.variables.overloaded);
+        if ~isempty(pos)
+          fprintf("%s:%d:%d - variable shadows function: %s\n", filename, lint.line, pos, ret.variables.overloaded{ind})
+        end
+      endif
     endif
     ## end check for shadowed functions
     ####################################
     
     ## check for unused variables
     ## --------------------------
-    if ~isempty(ret.variables.used)
-      if any(UL == 1) # check only when a variable is only used one time
-        pos = cell2mat(strfind(lineOfCode, ret.variables.used(UL == 1)));
-        if ~isempty(pos)
-          fprintf("%s:%d:%d - variable is assigned but never used: %s\n", filename, lint.line, pos, ret.variables.used{UL == 1})
+    if (p.Results.UnusedVar)
+      if ~isempty(ret.variables.used)
+        if any(UL == 1) # check only when a variable is only used one time
+          pos = cell2mat(strfind(lineOfCode, ret.variables.used(UL == 1)));
+          if ~isempty(pos)
+            fprintf("%s:%d:%d - variable is assigned but never used: %s\n", filename, lint.line, pos, ret.variables.used{UL == 1})
+          endif
         endif
       endif
     endif
